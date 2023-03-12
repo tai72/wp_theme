5fff9f6a807612075c979b36941588556bee8459
tai72 <yamashi7227.04020105.7227@outlook.jp>
2023-03-12 13:00:18 +0900

    [fix]記事一覧ページのchrome対応

--- portfolio/functions.php
+++ portfolio/functions.php
@@ -3,3 +3,13 @@ function portfolio_setup() {
     add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'portfolio_setup' );
+
+function new_excerpt_mblength($length) {
+    return 70;
+}
+add_filter( 'excerpt_mblength', 'new_excerpt_mblength' );
+
+function twpp_change_excerpt_more($more) {
+    return '....';
+}
+add_filter( 'excerpt_more', 'twpp_change_excerpt_more' );
