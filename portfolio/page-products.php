dc903f5febcaa66ea0ea80b5144aa8ee82a633bc
tai72 <yamashi7227.04020105.7227@outlook.jp>
2023-03-15 18:57:41 +0900

    [fix]CSSの共通項目を一元化

--- portfolio/page-products.php
+++ portfolio/page-products.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>tai`s portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/mystyle_common.css">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/page-products.css">
 
         <!-- jQuery読み込み -->
